a8774bb178792e4b95db5dd5236171a146b724de
jonhyun <godaiys@hotmail.com>
2018-07-26 15:22:39 -0400

    TabBarCustom

---
+++ MonRestoDuMonde/Controller/MonTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  MonTabBarController.swift
+//  MonRestoDuMonde
+//
+//  Created by MyMac on 18-07-26.
+//  Copyright © 2018 Apex. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+
+
+}
---
+++ MonRestoDuMonde/Utility/Constantes.swift
@@ -0,0 +1,13 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by MyMac on 18-07-26.
+//  Copyright © 2018 Apex. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240/255, green: 240 / 255, blue: 240 / 255, alpha: 1)
