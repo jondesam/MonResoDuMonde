d1ae9a10a88b84fda9ad325423cd5d273976d23b
jonhyun <godaiys@hotmail.com>
2018-07-26 15:02:08 -0400

    Ajout des controleurs principaux

---
+++ MonRestoDuMonde/Controller/MenuController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by MyMac on 18-07-26.
+//  Copyright © 2018 Apex. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
