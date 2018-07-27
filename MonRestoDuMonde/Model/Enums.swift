de01445b707db48213f75f04cea37538e6c27eb8
jonhyun <godaiys@hotmail.com>
2018-07-27 08:47:17 -0400

    Class Plat

---
+++ MonRestoDuMonde/Model/Enums.swift
@@ -0,0 +1,16 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by MyMac on 18-07-27.
+//  Copyright © 2018 Apex. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+    
+}
