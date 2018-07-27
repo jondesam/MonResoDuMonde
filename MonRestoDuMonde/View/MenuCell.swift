c7eae32826b3c442b36ca7ca3ea9f1c395307f77
jonhyun <godaiys@hotmail.com>
2018-07-27 08:24:45 -0400

    Ajout CollectionView

---
+++ MonRestoDuMonde/View/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by MyMac on 18-07-26.
+//  Copyright © 2018 Apex. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
